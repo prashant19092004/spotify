95e16a930b5349345f79f83cb7e50ee6b22d981a
Prashant Kumar <prashantkumargupta998@gmail.com>
2024-08-30 01:32:15 +0530

    application theme done

---
+++ lib/core/configs/theme/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xff42C83C);
+  static const lightBackground = Color(0xffF2F2F2);
+  static const darkBackground = Color(0xff0D0C0C);
+  static const gray = Color(0xffBEBEBE);
+}
