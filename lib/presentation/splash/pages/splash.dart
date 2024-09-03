537aeacecc8fdce1681f1c5c32eb3d57832783f2
Prashant Kumar <prashantkumargupta998@gmail.com>
2024-09-04 00:17:30 +0530

    get started page done

--- lib/presentation/splash/pages/splash.dart
+++ lib/presentation/splash/pages/splash.dart
@@ -1,9 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:spotify/core/configs/assets/app_vectors.dart';
+import 'package:spotify/presentation/intro/get_started.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends StatefulWidget {
   const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+
+  @override
+  void initState(){
+    super.initState();
+    redirect();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,4 +28,15 @@ class SplashPage extends StatelessWidget {
       ),
     );
   }
+
+  Future<void> redirect() async{
+    await Future.delayed(const Duration(seconds: 2));
+    Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+            builder: (BuildContext context) => GetStarted()
+        )
+    );
+  }
 }
+
