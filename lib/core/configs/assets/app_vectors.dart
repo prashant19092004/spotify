d880890c01d0ddc2209f6bf68851c35faa1ba914
Prashant Kumar <prashantkumargupta998@gmail.com>
2024-09-05 01:14:11 +0530

    Choose Mode page done

--- lib/core/configs/assets/app_vectors.dart
+++ lib/core/configs/assets/app_vectors.dart
@@ -4,4 +4,6 @@ class AppVectors{
   static const String format = '.svg';
 
   static const String logo = '${basePath}spotify_logo$format';
+  static const String sun = '${basePath}sun$format';
+  static const String moon = '${basePath}moon$format';
 }
